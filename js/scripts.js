d0b01bbd2bdd30ffdfe21bde9999bb7e8ee8aaed
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-09-10 12:35:33 -0300

    Adding the header

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+/* Changing the header background if it is a desktop screen or a mobile screen */
+const headerBg = document.querySelector(".hero-image");
+
+const bgDesktop = () => {
+  headerBg.src = "./img/main_background_desktop.jpg";
+};
+
+const bgMobile = () => {
+  headerBg.src = "./img/main_background_mobile.jpg";
+};
+
+const isMobile = () => {
+  if (window.screen.width < 600) {
+    return true;
+  }
+  return false;
+};
+
+const changeBg = () => {
+  if (isMobile()) {
+    return bgMobile();
+  }
+  return bgDesktop();
+};
+
+changeBg();
