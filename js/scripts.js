83dd787d0a8b6118eec90d76dc7037114a6ac40f
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-09-11 20:52:32 -0300

    Changing navbar

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 
 const body = document.body;
 const html = document.documentElement;
-const topNavBar = document.querySelector("#top-nav-bar");
+const topNavBar = document.querySelector("#nav-top");
 
 const scrollFunction = () => {
   if (body.scrollTop > 50 || html.scrollTop > 50) {
@@ -15,4 +15,29 @@ const scrollFunction = () => {
   return;
 };
 
-window.onscroll = () => scrollFunction();
+// window.onscroll = () => scrollFunction();
+
+/* Open Navbar menu */
+const mobileMenu = document.querySelector(".mobile-menu");
+const navList = document.querySelector(".nav-mobile");
+const navLinks = document.querySelectorAll(".nav-mobile .nav-item");
+
+const animateLinks = () => {
+  navLinks.forEach((link, index) => {
+    let time = (index / 7) * 0.3;
+    link.style.animation
+      ? (link.style.animation = "")
+      : (link.style.animation = `navLinksFade 0.5s ease forwards ${time}s`);
+    link.addEventListener("click", toggleMobileMenu);
+  });
+};
+
+const toggleMobileMenu = (event) => {
+  navList.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+  animateLinks();
+};
+
+if (mobileMenu) {
+  mobileMenu.addEventListener("click", toggleMobileMenu);
+}
